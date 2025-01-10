b31bad81c1ac53f3cb505a7e2441c3c2cf4d7180
Anand <pratyushanan@gmail.com>
2025-01-10 18:56:19 +0530

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,18 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by student on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+            
+        }
+    }
+}
